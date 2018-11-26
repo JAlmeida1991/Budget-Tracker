3d69011332810a539f02cdd2d52e26cc29b13e12
Jonathan <jalmeida0291@gmail.com>
2018-11-25 21:25:58 -0500

    added word-break for each li and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Budget Tracker
 
 An application created with Create-React-App CLI and Bootstrap
+
+Current draft can be found at: https://budget-tracker.netlify.com/
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import format from "date-fns/format";
 
 const Header = props => (
-  <header className="text-center mt-2 mb-2 header">
+  <header className="text-center m-1 header">
     <h1>Budget Tracker for {format(new Date(), "MMMM Do, YYYY")}</h1>
   </header>
 );
